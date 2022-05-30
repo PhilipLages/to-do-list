2c09e1cbb53476a7c4ad092494af351619e5fb18
Philip Lages <psilvaelages@gmail.com>
2022-05-30 17:45:40 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const toDoList = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 const listItems = document.getElementsByClassName('item');
 
+// Requisito 8:
 function changeItemsBackgroudColor(e) {
   for (const item of listItems) {
     item.style.backgroundColor = 'white';
@@ -24,13 +25,42 @@ function changeItemsBackgroudColor(e) {
   }
 }
 
+// Requisito 9:
+function createCompletedEvent(e) {
+  for (const item of listItems) {
+    item.style.textDecoration = 'none';
+    e.target.style.textDecoration= 'line-through solid black';
+    item.classList.remove('completed');
+    e.target.classList.add('completed');
+  }
+};
+
+function removeCompleted(e) {
+  for (const item of listItems) { 
+    if (item.style.textDecoration === 'line-through solid black')  
+    e.target.style.textDecoration = 'none';
+  }
+}
+
 listButton.addEventListener('click', () => {
   toDoList.appendChild(createNewTag('Li', 'class', 'item'));
   toDoList.lastElementChild.innerText = input.value;
   toDoList.lastElementChild.addEventListener('click', changeItemsBackgroudColor);
+  toDoList.lastElementChild.addEventListener('dblclick', createCompletedEvent);
+  toDoList.lastElementChild.addEventListener('dblclick', removeCompleted);
 });
 
 listButton.addEventListener('click', () => {
   console.log(input.value);
   input.value = '';
 });
+
+// Requisito 10:
+function createEraseBtn() {
+  container.appendChild(createNewTag('button', 'id', 'apaga-tudo'));
+  container.lastElementChild.innerText = 'Apagar tarefas';
+  container.lastElementChild.addEventListener('click', () => {
+    toDoList.innerHTML = ' ';
+  })
+};
+createEraseBtn();
