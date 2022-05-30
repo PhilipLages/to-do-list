435f29421da9a9dba12629d1e625ce51e5f1cd05
Philip Lages <psilvaelages@gmail.com>
2022-05-30 12:09:40 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,9 @@
+function createNewTag(tag, element, value) {         
+  const createTag = document.createElement(tag);        
+  createTag.setAttribute(element, value);    
+  return createTag;
+}
+
+function addNewTag()
+// Requisitos 1, 2 e 3 em HTML
+// Requisito 4:
